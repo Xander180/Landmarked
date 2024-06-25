e25fbc943ebd22d348cd9917c5922e759d9d94a1
binaryPhishing <wilson.ramirez98@gmail.com>
2024-06-25 14:49:30 -0400

    Initial Commit

---
+++ Landmarked/LandmarkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkedApp.swift
+//  Landmarked
+//
+//  Created by Wilson Ramirez on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
