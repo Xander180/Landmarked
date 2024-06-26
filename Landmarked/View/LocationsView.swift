61904ecd1ca947ed072d9794f1d542cee018d22c
binaryPhishing <wilson.ramirez98@gmail.com>
2024-06-26 10:14:44 -0400

    [Feature] Create LocationsView + LocationsViewModel, Create ID variable in Location model to conform to Identifiable, update LandmarkedApp.swift to preload LocationsViewModel as well as load into LocationsView first

---
+++ Landmarked/View/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  Landmarked
+//
+//  Created by Wilson Ramirez on 6/26/24.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
