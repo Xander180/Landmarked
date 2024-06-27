60ae9edf31ec39eee2638f8231928f4e1f328b2a
binaryPhishing <wilson.ramirez98@gmail.com>
2024-06-27 14:56:38 -0400

    [Feature][Clean] Create custom pin for each location on map, cleaned up code in LocationsView

---
+++ Landmarked/View/LocationMapAnnotationView.swift
@@ -0,0 +1,41 @@
+//
+//  LocationMapAnnotationView.swift
+//  Landmarked
+//
+//  Created by Wilson Ramirez on 6/27/24.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundStyle(.white)
+                .padding(6)
+                .background(.accent)
+                .clipShape(RoundedRectangle(cornerRadius: 36))
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.accent)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom, 40)
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        LocationMapAnnotationView()
+    }
+}
